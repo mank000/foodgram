32f930c45db8caac60b1a380f68ec78e2824e3a8
mank000 <kozmin_arti@mail.ru>
2024-07-25 01:30:01 +0700

    Fix_page_size

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -105,7 +105,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 6,
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
         'rest_framework.filters.SearchFilter',
